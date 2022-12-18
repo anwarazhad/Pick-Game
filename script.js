dbc11936d53539f1465cd551c2e868c449f22522
susshi <anwarazhad00@gmail.com>
2022-12-18 22:05:01 +0800

    Make score player 0 and player 1 to zero

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 'use strict';
+
+// Selecting elements 
+// if select by id use #id
+// if select by class use .class
+const score0El = document.querySelector('#score--0');
+const score1El = document.getElementById('score--1');
+const diceEl = document.querySelector('.dice');
+
+// Starting condition
+score0El.textContent = 0;
+score1El.textContent = 0;
+diceEl.classList.add('hidden');
